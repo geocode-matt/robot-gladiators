8e593a24264ec84c0ea3f30e55a1e1abc99ab6df
geocode-matt <mlindenx@gmail.com>
2020-06-04 15:52:56 -0500

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -16,6 +16,13 @@ console.log(enemyNames);
 var enemyHealth = 40;
 var enemyAttack = 12;
 
+// function to generate a random numeric value
+var randomNumber = function(min, max) {
+  var value = Math.floor(Math.random() * (max - min + 1) + min);
+
+  return value;
+};
+
 var fight = function (enemyName) {
     while (playerHealth > 0 && enemyHealth > 0) {
         // ask user if they'd liked to fight or run
@@ -28,14 +35,17 @@ var fight = function (enemyName) {
             if (confirmSkip) {
                 window.alert(playerName + ' has decided to skip this fight. Goodbye!');
                 // subtract money from playerMoney for skipping
-                playerMoney = playerMoney - 10;
+                playerMoney = Math.max(0, playerMoney - 10);
                 console.log("playerMoney", playerMoney)
                 break;
             }
         }
 
-        // remove enemy's health by subtracting the amount set in the playerAttack variable
-        enemyHealth = enemyHealth - playerAttack;
+        // generate random damage value based on player's attack power
+      var damage = randomNumber(playerAttack - 3, playerAttack);
+
+      enemyHealth = Math.max(0, enemyHealth - damage);
+
         console.log(
             playerName + ' attacked ' + enemyName + '. ' + enemyName + ' now has ' + enemyHealth + ' health remaining.'
         );
@@ -49,8 +59,12 @@ var fight = function (enemyName) {
         } else {
             window.alert(enemyName + ' still has ' + enemyHealth + ' health left.');
         }
-        // remove players's health by subtracting the amount set in the enemyAttack variable
-        playerHealth = playerHealth - enemyAttack;
+      
+        // generate random damage value based on enemy's attack power
+      var damage = randomNumber(enemyAttack - 3, enemyAttack);
+
+      playerHealth = Math.max(0, playerHealth - damage);
+
         console.log(
             enemyName + ' attacked ' + playerName + '. ' + playerName + ' now has ' + playerHealth + ' health remaining.'
         );
@@ -122,7 +136,7 @@ var startGame = function() {
   
         var pickedEnemyName = enemyNames[i];
   
-        enemyHealth = 50;
+        enemyHealth = randomNumber(40, 60);
   
         fight(pickedEnemyName);
 
@@ -163,3 +177,4 @@ if (playAgainConfirm) {
 else {
   window.alert("Thank you for playing Robot Gladiators! Come back soon!");
 };
+
